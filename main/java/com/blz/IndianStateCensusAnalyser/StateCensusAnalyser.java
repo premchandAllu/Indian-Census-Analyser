44c71007282a7a0593a8140d1cfa28a0a1c4862d
premchandAllu <premchandallu1999@gmail.com>
2020-10-21 10:07:26 +0530

    Added: Indian State Census Analyser

---
+++ main/java/com/blz/IndianStateCensusAnalyser/StateCensusAnalyser.java
@@ -0,0 +1,14 @@
+package com.blz.IndianStateCensusAnalyser;
+
+import java.util.logging.Logger;
+
+public class StateCensusAnalyser {
+
+	public static void main(String[] args) {
+		
+	  Logger log = Logger.getLogger(StateCensusAnalyser.class.getName());
+	  log.info("Welcome to Indian State Census Program");
+	  
+	}
+
+}
